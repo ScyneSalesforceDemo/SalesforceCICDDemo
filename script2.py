9c14dced8da6758890812903f9ac1e4fe1890d36
Rohit Mohandas <rohit.mohandas@health.telstra.com>
2024-06-21 10:34:20 +1000

    cHANGES

--- script2.py
+++ script2.py
@@ -62,17 +62,17 @@ for deltafile in merged_list:
     print ("This is from Loop "+deltafile)
     count=count+1
     print(count) 
-print("I am now trying to create in the directory")
-print("This is printing from " +deltafile)
-print(os.path.exists(deltafile))
-if os.path.exists(deltafile):
-            #destination_file = os.path.join(destination_dir, os.path.basename(deltafile))
-            print("This is how relative path looks" +os.path.relpath(deltafile))
-            newsource = os.path.join(Baseurl,os.path.relpath(deltafile))
-            print("This is the Path which I want to Look for" +newsource)
-            print("This is the dir name for the file"  +os.path.dirname(deltafile))
-            destpath= os.path.join(destination_dir,os.path.dirname(deltafile))
-            print("This is how absolute path looks" +destpath)
-            if not os.path.exists(destpath):
-                os.makedirs(destpath)
-            shutil.copy2(newsource, destpath)    
+    print("I am now trying to create in the directory")
+    print("This is printing from " +deltafile)
+    print(os.path.exists(deltafile))
+    if os.path.exists(deltafile):
+                #destination_file = os.path.join(destination_dir, os.path.basename(deltafile))
+                print("This is how relative path looks" +os.path.relpath(deltafile))
+                newsource = os.path.join(Baseurl,os.path.relpath(deltafile))
+                print("This is the Path which I want to Look for" +newsource)
+                print("This is the dir name for the file"  +os.path.dirname(deltafile))
+                destpath= os.path.join(destination_dir,os.path.dirname(deltafile))
+                print("This is how absolute path looks" +destpath)
+                if not os.path.exists(destpath):
+                    os.makedirs(destpath)
+                shutil.copy2(newsource, destpath)    
